67b6c5587e55ae6ced0965dd5cf9947a380d1551
Chris <christopherhong8@gmail.com>
2024-02-04 15:32:31 -0800

    Restructuring files

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ import pandas as pd
 
 st.header("Streamlit Deployment Test", False, divider="rainbow")
 
-cities_df = pd.read_csv("../assets/cities.csv", dtype=str)
-oscarsm_df = pd.read_csv("../assets/oscar_age_male.csv", dtype=str)
-oscarsf_df = pd.read_csv("../assets/oscar_age_female.csv", dtype=str)
+cities_df = pd.read_csv("./assets/cities.csv", dtype=str)
+oscarsm_df = pd.read_csv("./assets/oscar_age_male.csv", dtype=str)
+oscarsf_df = pd.read_csv("./assets/oscar_age_female.csv", dtype=str)
 
 st.subheader("Example Cities")
 st.write(cities_df)
